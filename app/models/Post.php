45767058d304d445f0eeb642233d034690e42490
davide <davidevischetti@gmail.com>
2022-07-29 18:10:00 +0200

    first commit

---
+++ app/models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Slugger;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use Slugger;
+    protected $fillable = [
+        'title', 'content', 'excerpt', 'category_id', 'image', 'slug', 'user_id'
+    ];
+
+    public function category() {
+        return $this->belongsTo('App\Models\Category');
+    }
+
+    public function tags() {
+        return $this->belongsToMany('App\Models\Tag');
+    }
+
+    public function user() {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function getRouteKeyName() {
+        return 'slug';
+    }
+}
---
+++ resources/js/front.js
@@ -0,0 +1,15 @@
+
+require('./bootstrap');
+
+
+
+window.Vue = require('vue');
+
+import App from './components/App.vue';
+
+
+const app = new Vue({
+    el: '#root',
+    render: h => h(App)
+});
+
